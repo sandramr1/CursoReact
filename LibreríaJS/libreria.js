7673a434465c5fcf3ece68e3a7ecaf8d3ddf1c19
sandramr1 <sandraa_so@hotmail.es>
2022-06-02 23:02:12 -0600

    se separan la libreria y el script

--- LibreríaJS/libreria.js
+++ LibreríaJS/libreria.js
@@ -7,19 +7,15 @@
 
 // Se construye lo anterior mediante las siguientes funciones para poder crear headers en HTML a través del div root.
 
-function createElement(elem, attr = {}){
+export function createElement(elem, attr = {}){
     let element = document.createElement(elem);
     element.id = attr.id;
     element.classList.add(attr.className);
     return element
 };
 
-function render(elem, DOMelem){
+export function render(elem, DOMelem){
     DOMelem.appendChild(elem);
 
 
 };
-const Header = createElement("header", { id: "mainHeader2", className: "main-header-2"})
-
-render(Header, document.getElementById("root"));
-
---
+++ LibreríaJS/script.js
@@ -0,0 +1,7 @@
+import  {createElement, render} from "./libreria.js"
+
+
+const Header = createElement("header", { id: "mainHeader2", className: "main-header-2"})
+
+render(Header, document.getElementById("root"));
+
